ce34786affa7e2d6ae8b801f8981118b13cd9f07
Gift <chanatinart.k@gmail.com>
2023-10-23 15:51:58 +0700

    create goal backend

--- server/controllers/goal.controller.js
+++ server/controllers/goal.controller.js
@@ -3,7 +3,27 @@ import Goal from "../models/goal.model.js";
 import User from "../models/user.model.js";
 
 const createGoal = asyncHandler(async (req, res) => {
-  res.status(200).send("create goal");
+  const { typeOfActivity, deadline, duration, energyBurn, distance, email } =
+    req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("User not fond");
+
+  const newGoal = await Goal.create({
+    userId: user._id,
+    typeOfActivity,
+    deadline,
+    duration,
+    energyBurn,
+    distance,
+  });
+
+  try {
+    const saveGoal = await newGoal.save();
+    res.status(201).json(saveGoal);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 const getGoals = asyncHandler(async (req, res) => {
